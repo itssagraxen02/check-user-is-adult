e92a35fb94a7b9f0e677944af970016ec4c56f90
itssagraxen02 <agrasenc2017@gmail.com>
2024-12-23 12:45:13 +0530

    Add files via upload

---
+++ agrasen.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main(){
+
+int age;
+printf("enter age");
+scanf("%d" , &age);
+
+if(age > 18) {
+printf("is adult /n");
+
+}
+else{
+printf("is not adult /n");
+}
+return 0;
+}
